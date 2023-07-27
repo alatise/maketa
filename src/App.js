bdb0a21d71592111285ad48ae92944f8c0c4d1a0
Alatise <fayobio17@gmail.com>
2023-07-27 12:41:25 +0100

    Worked on the entire screen layout and nav web

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
-function App() {
-  return <div className="text-3xl">Just getting started</div>;
+// import { FaShoppingCart } from "react-icons/fa";
+
+function AppMaketa() {
+  return (
+    <>
+      <div className="px-16 py-8">
+        <nav className="flex flex-row">
+          <div className="flex ">
+            <img src={process.env.PUBLIC_URL + "/assets/logo.svg"} alt="Logo" />
+          </div>
+          <div className="flex gap-8">
+            <p>Collections</p>
+            <p>Men</p>
+            <p>Women</p>
+            <p>About</p>
+            <p>Contacts</p>
+          </div>
+          <div className="flex gap-8">
+            <img
+              src={process.env.PUBLIC_URL + "/assets/icon-cart.svg"}
+              alt=""
+              className=""
+            />
+            <img
+              src={process.env.PUBLIC_URL + "/assets/images/image-avatar.png"}
+              alt=""
+              className="w-8 h-8"
+            />
+          </div>
+        </nav>
+      </div>
+    </>
+  );
 }
 
-export default App;
+export default AppMaketa;
