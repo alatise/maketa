decedcd256f2ac9d116488872ba718a74eb20d87
Alatise <fayobio17@gmail.com>
2023-07-28 03:54:20 +0100

    Finished with all contents, just logic and styling left

--- src/App.js
+++ src/App.js
@@ -1,36 +1,78 @@
-// import { FaShoppingCart } from "react-icons/fa";
+import React from "react";
+import { Products } from "./components/productData";
 
 function AppMaketa() {
   return (
-    <>
-      <div className="px-16 py-8">
-        <nav className="flex flex-row">
-          <div className="flex ">
-            <img src={process.env.PUBLIC_URL + "/assets/logo.svg"} alt="Logo" />
-          </div>
-          <div className="flex gap-8">
-            <p>Collections</p>
-            <p>Men</p>
-            <p>Women</p>
-            <p>About</p>
+    <div className="px-36 py-8">
+      <div className=" flex justify-between items-center">
+        <div className="flex items-center">
+          <img src={process.env.PUBLIC_URL + "/assets/logo.svg"} alt="Logo" />
+          <nav className="flex ml-12 text-[#BABCC4] gap-6">
+            <p className="">Collections</p>
+            <p className="">Men</p>
+            <p className="">Women</p>
+            <p className="">About</p>
             <p>Contacts</p>
-          </div>
-          <div className="flex gap-8">
-            <img
-              src={process.env.PUBLIC_URL + "/assets/icon-cart.svg"}
-              alt=""
-              className=""
-            />
+          </nav>
+        </div>
+        <div className="flex gap-10 items-center">
+          <img
+            src={process.env.PUBLIC_URL + "/assets/icon-cart.svg"}
+            alt="Cart"
+            className=""
+          />
+          <div className="rounded-full border border-[#CE651B]">
             <img
               src={process.env.PUBLIC_URL + "/assets/images/image-avatar.png"}
-              alt=""
-              className="w-8 h-8"
+              alt="Avatar"
+              className="w-11 h-11 rounded-full"
             />
           </div>
-        </nav>
+        </div>
       </div>
-    </>
+      <div className="border border-b-[#BABCC4] mt-8"></div>
+      <main className="px-8 mt-20 flex gap-10">
+        <section className="flex w-1/2 ">
+          {Products.map((product, index) => (
+            <div key={index}>
+              <div>
+                <img src={product.original} alt={`Product ${index + 1}`} />
+              </div>
+              <div>
+                <img src={product.thumbnail} alt={`Product ${index + 1}`} />
+              </div>
+            </div>
+          ))}
+        </section>
+        <section>
+          <p>Sneaker Company</p>
+          <p className="text-xl">Fall Limited Edition Sneakers</p>
+          <article className="text-sm">
+            These low-profile sneakers are your perfect casual wear companion.
+            Featuring a durable rubber outer sole, they’ll withstand everything
+            the weather can offer.
+          </article>
+          <div className="flex">
+            <p>$125.00</p>
+            <p>50%</p>
+          </div>
+          <p>$250.00</p>
+          <div className="flex">
+            <button className="">Add to cart</button>
+          </div>
+        </section>
+      </main>
+    </div>
   );
 }
 
 export default AppMaketa;
+
+// Your users should be able to:
+
+// - View the optimal layout for the site depending on their device's screen size
+// - See hover states for all interactive elements on the page
+// - Open a lightbox gallery by clicking on the large product image
+// - Switch the large product image by clicking on the small thumbnail images
+// - Add items to the cart
+// - View the cart and remove items from it
---
+++ src/components/productData.js
@@ -0,0 +1,24 @@
+const Products = [
+  {
+    original: process.env.PUBLIC_URL + "/assets/images/image-product-1.jpg",
+    thumbnail:
+      process.env.PUBLIC_URL + "/assets/images/image-product-1-thumbnail.jpg",
+  },
+  {
+    original: process.env.PUBLIC_URL + "/assets/images/image-product-2.jpg",
+    thumbnail:
+      process.env.PUBLIC_URL + "/assets/images/image-product-2-thumbnail.jpg",
+  },
+  {
+    original: process.env.PUBLIC_URL + "/assets/images/image-product-3.jpg",
+    thumbnail:
+      process.env.PUBLIC_URL + "/assets/images/image-product-3-thumbnail.jpg",
+  },
+  {
+    original: process.env.PUBLIC_URL + "/assets/images/image-product-4.jpg",
+    thumbnail:
+      process.env.PUBLIC_URL + "/assets/images/image-product-4-thumbnail.jpg",
+  },
+];
+
+export { Products };
